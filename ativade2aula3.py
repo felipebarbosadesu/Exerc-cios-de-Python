6af9c16e481ea9bd3619dad7115f47419d99fb7f
Felipe Barbosa <85811327+felipebarbosadesu@users.noreply.github.com>
2025-08-24 15:13:03 -0300

    Laço para repetição de 3 números

--- ativade2aula3.py
+++ ativade2aula3.py
@@ -1,6 +1,8 @@
-#laço para repetição de 3 numeros 
-
-for i in range(0,3):
-    valor = int(input('Digite um numero: '))
+# Exemplo de Laço com 3 Números em Python
+for i in range(3):
+    valor = int(input('Digite um número: '))
     
-    if 
+    if valor % 2 == 0:
+        print(f'O número {valor} é PAR')
+    else:
+        print(f'O número {valor} é ÍMPAR')
